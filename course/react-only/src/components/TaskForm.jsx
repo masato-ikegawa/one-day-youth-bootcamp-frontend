7c8ed704a8d7f88608a5b5d408edd2631f2c51e3
matton <masato_ikegawa612400@yahoo.co.jp>
2021-12-13 16:32:37 +0900

    first commit

--- course/react-only/src/components/TaskForm.jsx
+++ course/react-only/src/components/TaskForm.jsx
@@ -1,13 +1,67 @@
 import React from 'react';
 
-export const TaskForm = () => {
+export const TaskForm = ({
+  tasks,
+  setTasks,
+  newTaskLabel,
+  setNewTaskLabel,
+}) => {
   // フォームの値を保持する
-
+  const handleNewTaskLabel = (e) => {
+    setNewTaskLabel(e.target.value);
+  };
   // Taskの登録
-
+  const handleAddTask = () => {
+    const newTask = { label: newTaskLabel, isDone: false }
+    setTasks([...tasks, newTask]);
+    setNewTaskLabel('');
+  };
   // 完了したTaskを削除する
+  const handleClearTasks = () => {
+    const newTasks = tasks.filter((task) => !task.isDone);
+    setTasks(newTasks);
+  };
+
+  // const checkLaboTasks = (task) => {
+  //   const _tag = '[Lab]'
+  //   const _label = task.label
+  //   const newTasks
+
+  // }
 
   return (
-    <></>
+    <>
+      <input
+        style = {{
+          width: '360px',
+          padding: '12px 8px',
+          borderRadius: '22px',
+          borderTop: '1px solid #aaa',
+          borderLeft: '1px solid #aaa',
+          borderRight: '2px solid #aaa',
+          borderBottom: '2px solid #aaa',
+          backgroundImage: 'none',
+          backgroundColor: 'yellow',
+          borderColor: 'whitesmoke',
+          fontSize: '16px',
+        }}
+        onChange={handleNewTaskLabel}
+        type="text"
+        value={newTaskLabel}
+        placeholder="Enter the task"
+      />
+      <button 
+      style = {{
+        color: '#fff',
+        backgroundColor: 'red',
+        borderBottom: '5px #9f000c',
+        fontSize: '20px',
+        borderRadius: '100vh',
+        borderColor: 'white',
+      }}
+      onClick={handleAddTask}>Add</button>
+      <br />
+      <button onClick={handleClearTasks}>Clear</button>
+    </>
   );
 };
--- course/react-only/src/components/TaskList.jsx
+++ course/react-only/src/components/TaskList.jsx
@@ -1,10 +1,30 @@
 import React from 'react';
+import { Task } from '../'
 
-export const TaskList = () => {
+export const TaskList = ({ tasks, setTasks }) => {
   // Taskの状態を切り替える
+  const handleCheckBox = (e, i) => {
+    const newTasks = tasks.map((task, _i) => {
+      return _i === i ? { ...task, isDone: e.target.checked } : task;
+    });
+    console.log(newTasks);
+    setTasks(newTasks);
+  };
 
   return (
-    <></>
+    <ul>
+      {tasks.map((task, index) => (
+        <li key={`todo-${index}`}>
+          {task.isDone ? <s>{task.label}</s> : task.label}
+          <input
+            style = {{marginLeft: '30px', transform: 'scale(2.5)'}}
+            onChange={(e) => handleCheckBox(e, index)}
+            type="checkbox"
+            checked={task.isDone}
+          />
+        </li>
+      ))}
+    </ul>
   );
 };
 
